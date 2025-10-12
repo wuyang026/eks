159914b24fc4d162cccabf6cc93e6160931d7f66
wuyang <yangwu1129@gmail.com>
2025-10-12 11:08:45 +0900

    cluster auth update

--- terraform-manifests/providers.tf
+++ terraform-manifests/providers.tf
@@ -1,11 +1,9 @@
 data "aws_eks_cluster" "cluster" {
   name       = module.eks.cluster_name
-  depends_on = [module.eks.cluster_name]
 }
 
 data "aws_eks_cluster_auth" "this" {
   name       = module.eks.cluster_name
-  depends_on = [module.eks.cluster_name]
 }
 
 provider "kubectl" {
