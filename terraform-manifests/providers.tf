a003c63843b58d836eecff49543c77308a0a04da
wuyang <yangwu1129@gmail.com>
2025-10-12 20:02:44 +0900

    provider kubectl update

--- terraform-manifests/providers.tf
+++ terraform-manifests/providers.tf
@@ -1,19 +1,29 @@
-data "aws_eks_cluster" "cluster" {
-  name       = module.eks.cluster_name
-  depends_on = [module.eks]
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name       = module.eks.cluster_name
-  depends_on = [module.eks]
-}
-
 provider "kubectl" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.this.token
-  load_config_file       = false
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
 }
+#data "aws_eks_cluster" "cluster" {
+#  name       = module.eks.cluster_name
+#  depends_on = [module.eks]
+#}
+
+#data "aws_eks_cluster_auth" "this" {
+#  name       = module.eks.cluster_name
+#  depends_on = [module.eks]
+#}
+
+#provider "kubectl" {
+#  host                   = data.aws_eks_cluster.cluster.endpoint
+#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#  token                  = data.aws_eks_cluster_auth.this.token
+#  load_config_file       = false
+#}
 
 #data "aws_caller_identity" "current" {}
 
