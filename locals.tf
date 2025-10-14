4f2e53aa490fe3250619264e59a493c85048d8ed
wuyang <yangwu1129@gmail.com>
2025-10-14 12:53:44 +0900

    subnet update

--- locals.tf
+++ locals.tf
@@ -2,8 +2,8 @@
 locals {
   name                    = "${var.project_name}-${var.environment}"
   cluster_name            = "${local.name}-eks-cluster"
-  node_class_name         = "${local.prefix_env}-nodeclass"
-  node_pool_name          = "${local.prefix_env}-nodepool"
+  node_class_name         = "${local.cluster_name}-nodeclass"
+  node_pool_name          = "${local.cluster_name}-nodepool"
   cluster_sg_name         = "${local.cluster_name}-terr-sg"
   node_sg_name            = "${local.cluster_name}-node-terr-sg"
 
