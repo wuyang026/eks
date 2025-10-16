8f9e94fa958e83ddaf9e63f9c44d7c29e6c8102e
wuyang <yangwu1129@gmail.com>
2025-10-16 10:20:23 +0900

    ecr update

--- ecr/ecr.tf
+++ ecr/ecr.tf
@@ -1,15 +1,14 @@
-# ECR repository作成
 resource "aws_ecr_repository" "repo" {
   for_each = toset(var.ecr_repo_names)
 
-  name = each.key
+  name = "${each.key}-${var.environment}"
 
   image_scanning_configuration {
     scan_on_push = true
   }
 
   tags = {
-    Name        = each.key
+    Name        = "${each.key}-${var.environment}"
     Environment = var.environment
   }
 }
